b4115149ac8bd6add781b3f3ebef9400472dcaea
Gabriel La Torre <lt.gabriel96@gmail.com>
2025-05-16 01:41:00 -0300

    use API

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -9,56 +9,19 @@ export class TodoService {
 
   constructor(private http: HttpClient) { }
 
-  mockedTodos: Todo[] = [
-    { id: 1, title: 'Todo 1', completed: false },
-    { id: 2, title: 'Todo 2', completed: true },
-    { id: 3, title: 'Todo 3', completed: false },
-    { id: 4, title: 'Todo 4', completed: true },
-    { id: 5, title: 'Todo 5', completed: false }
-  ];
   fetchTodos(): Promise<Todo[]> {
-    // return firstValueFrom(this.http.get<Todo[]>(this.apiUrl));
-
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(this.mockedTodos);
-      }, 200);
-    })
+    return firstValueFrom(this.http.get<Todo[]>(this.apiUrl));
   }
 
   createTodo(todo: Partial<Todo>): Promise<Todo> {
-    // return firstValueFrom(this.http.post<Todo>(this.apiUrl, todo));
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        const newTodo = { ...todo, id: this.mockedTodos.length + 1 } as Todo;
-        this.mockedTodos.push(newTodo);
-        resolve(newTodo);
-      }, 200);
-    });
+    return firstValueFrom(this.http.post<Todo>(this.apiUrl, todo));
   }
 
-  updateTodo(todo: Partial<Todo>): Promise<Todo> {
-    // return firstValueFrom(this.http.put<Todo>(`${this.apiUrl}/${todo.id}`, todo));
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        const index = this.mockedTodos.findIndex(t => t.id === todo.id);
-        if (index !== -1) {
-          this.mockedTodos[index] = { ...this.mockedTodos[index], ...todo } as Todo;
-          resolve(this.mockedTodos[index]);
-        } else {
-          resolve(todo as Todo);
-        }
-      }, 200);
-    });
+  updateTodo(todo: Partial<Todo>, id: string): Promise<Todo> {
+    return firstValueFrom(this.http.put<Todo>(`${this.apiUrl}/${id}`, todo));
   }
 
-  deleteTodo(id: number): Promise<void> {
-    // return firstValueFrom(this.http.delete<void>(`${this.apiUrl}/${id}`));
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        this.mockedTodos = this.mockedTodos.filter(todo => todo.id !== id);
-        resolve();
-      }, 200);
-    });
+  deleteTodo(id: string): Promise<void> {
+    return firstValueFrom(this.http.delete<void>(`${this.apiUrl}/${id}`));
   }
 }
--- src/app/stores/todo.store.ts
+++ src/app/stores/todo.store.ts
@@ -2,15 +2,28 @@ import { Injectable, signal, computed } from '@angular/core';
 import { Todo } from '../models/todo.model';
 import { TodoService } from '../services/todo.service'
 
+type FilterType = 'all' | 'completed' | 'pending';
+
 @Injectable({ providedIn: 'root' })
 export class TodoStore {
     private _todos = signal<Todo[]>([]);
     private _selectedTodo = signal<Todo | null>(null);
     private _loading = signal<boolean>(true);
+    private _filter = signal<FilterType>('all');
 
-    readonly todos = computed(() => this._todos());
+    readonly todos = computed(() => {
+        const filter = this._filter();
+        const todos = this._todos();
+
+        switch (filter) {
+            case 'completed': return todos.filter(t => t.completed);
+            case 'pending': return todos.filter(t => !t.completed);
+            default: return todos;
+        }
+    });
     readonly selectedTodo = computed(() => this._selectedTodo());
     readonly loading = computed(() => this._loading());
+    readonly currentFilter = computed(() => this._filter());
 
     constructor(private todoService: TodoService) { }
 
@@ -26,12 +39,12 @@ export class TodoStore {
         this._todos.update(t => [...t, created]);
     }
 
-    async update(update: Partial<Todo>): Promise<void> {
-        const updated = await this.todoService.updateTodo(update);
+    async update(update: Partial<Todo>, id: string): Promise<void> {
+        const updated = await this.todoService.updateTodo(update, id);
         this._todos.update(t => t.map(x => x.id === updated.id ? updated : x));
     }
 
-    async delete(id: number): Promise<void> {
+    async delete(id: string): Promise<void> {
         await this.todoService.deleteTodo(id);
         this._todos.update(t => t.filter(x => x.id !== id));
     }
@@ -42,5 +55,9 @@ export class TodoStore {
 
     clearSelection(): void {
         this._selectedTodo.set(null);
+    }
+
+    setFilter(filter: FilterType): void {
+        this._filter.set(filter);
     }
 }
